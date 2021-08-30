47b1881e1bd2bafa9277c4de65b0e87c2bfe81ec
hjmin <hjmin@rsupport.com>
2021-08-30 14:23:53 +0900

    s4 end

---
+++ src/main/java/io/test/security/handler/AjaxAuthenticationFailureHandlerCustom.java
@@ -0,0 +1,36 @@
+package io.test.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AjaxAuthenticationFailureHandlerCustom implements AuthenticationFailureHandler {
+
+  private ObjectMapper objectMapper = new ObjectMapper();
+
+  @Override
+  public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+    String message = "Invalid Username or Password";
+
+    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+    if (exception instanceof DisabledException) {
+      message = "Locked";
+    } else if (exception instanceof CredentialsExpiredException) {
+      message = "Expired password";
+    }
+
+    objectMapper.writeValue(response.getWriter(), message);
+  }
+
+}
